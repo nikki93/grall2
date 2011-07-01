faf64ffd029d5f717e36f7ecc152fa9b2c4733bc
nikki93 <s.nikhilesh@gmail.com>
2011-07-02 02:40:07 +0300

    Make SpeedBoost multiply linear and not angular velocity

--- data/Python/Prefabs/Prefab_SpeedBoost.py
+++ data/Python/Prefabs/Prefab_SpeedBoost.py
@@ -2,10 +2,14 @@
 #------------------------
 
 import GraLL2
+import Ngf
 
 def pickedUp(self):
     #Multiply his rotational speed by whatever multiplier we've stored.
-    GraLL2.player.setAngularVelocity(
-        GraLL2.player.getAngularVelocity() * 
-            float(self.getProperty("multiplier", 0, "7"))
-    )
+    v = GraLL2.player.getLinearVelocity() * float(self.getProperty("multiplier", 0, "3"))
+
+    #Remove y component if needed.
+    if (parseBool(self.getProperty("removeY", 0, "no"))):
+        v.y = 0
+
+    GraLL2.player.setLinearVelocity(v)
