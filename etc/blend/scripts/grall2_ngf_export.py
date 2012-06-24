e50f054ec7a031aac013470ed7408208c8fa2ccf
nikki93 <s.nikhilesh@gmail.com>
2012-06-23 22:20:21 -0400

    Actually put new Blender 2.63 export script in git

--- etc/blend/scripts/grall2_ngf_export.py
+++ etc/blend/scripts/grall2_ngf_export.py
@@ -1,116 +1,191 @@
-#!BPY
+import bpy
+import functools
+import subprocess
 
-"""
-Name: 'GraLL2 NGF Level (.ngf)'
-Blender: 237
-Group: 'Export'
-Tooltip: 'Exports a level to NGF format (with GraLL 2 layer-dimension modifications)'
-"""
+bl_info = {
+    "name": "GraLL 2 NGF Exporter",
+    "description": "Exports a GraLL 2 NGF level",
+    "author": "Nikhilesh Sigatapu",
+    "version": (1, 0),
+    "blender": (2, 63, 0),
+    "location": "File > Export",
+    "warning": "", # used for warning icon and text in addons panel
+    "category": "Import-Export"
+}
 
-import Blender
+GRALL2_COMMAND='/home/nikki/Development/Projects/grall2/bin/debug/GraLL2_d'
+GRALL2_CWD='/home/nikki/Development/Projects/grall2/bin/debug/'
 
-def write(filename):
-    out = file(filename, "w")
+class GraLL2RunGame(bpy.types.Operator):
+    bl_idname = "export.grall2_run_game"
+    bl_label = "Run GraLL 2 game with currently edited level"
 
-    materials = Blender.Material.Get()
-    numMat = 0
+    @classmethod
+    def poll(cls, context):
+        return True
 
-    scenes = Blender.Scene.Get()
+    def execute(self, context):
+        subprocess.Popen([GRALL2_COMMAND, context.scene.name],
+                cwd = GRALL2_CWD)
+        return {'FINISHED'}
 
-    for scn in scenes:
-        out.write("ngflevel %s\n{\n" % (scn.getName()))
+class GraLL2OgreMeshes(bpy.types.Operator):
+    bl_idname = "export.grall2_ngf_ogre_meshes"
+    bl_label = "Export GraLL 2 NGF Level and run Ogre Mesh Exporter"
 
-        numBrush = 0
+    @classmethod
+    def poll(cls, context):
+        return True
 
-        for object in scn.getChildren():
-            object.removeProperty("dimension1")
-            object.removeProperty("dimension2")
-                
-            object.select(False)
+    def execute(self, context):
+        bpy.ops.export.grall2_ngf()
+        #bpy.ops.ogre3d.export() # doesn't work at the moment
 
-            objType = object.getProperty("type").getData()
-            objName = object.getProperty("name").getData()
-            objPos = object.getLocation()
-            objRot = object.getMatrix().toQuat()
+        return {'FINISHED'}
 
-            out.write("\tobject\n\t{\n")
-            out.write("\t\ttype %s\n" % (objType))
-            out.write("\t\tname %s\n" % (objName))
-            out.write("\t\tposition %f %f %f\n" % (objPos[0], objPos[2], -objPos[1]))
-            out.write("\t\trotation %f %f %f %f\n" % (objRot[0], objRot[1], objRot[3], -objRot[2]))
+class GraLL2Exporter(bpy.types.Operator):
+    bl_idname = "export.grall2_ngf"
+    bl_label = "Export GraLL 2 NGF Level"
 
-            objProps = object.getAllProperties()
-            numProps = 0
-            propStr = "\n\t\tproperties\n\t\t{\n"
-            
-            #Add dimension data.
-            propStr = propStr + ("\t\t\tdimensions %d\n" % object.Layers)
-                            
-            for prop in objProps:
-                if prop.getName() != "type" and prop.getName() != "name" and prop.getName() != "dimension1" and prop.getName != "dimension2":
-                    numProps = numProps + 1
-                    key = prop.getName()
-                    params = prop.getData()
-                    data = ""
+    filepath = bpy.props.StringProperty(subtype="FILE_PATH")
 
-                    if prop.getName() == "isBrush":
-                        nameToSet = scn.getName() + "_b" + str(numBrush)
-                        object.setName(nameToSet)
-                        object.getData(0,1).name = (nameToSet)
+    def __init__(self):
+        filepath = ''
 
-                        numMat = 0
-                        materials = object.getData(0,1).materials
+    @classmethod
+    def poll(cls, context):
+        return True
 
-                        for mat in materials:
-                            mat.setName(nameToSet + "_m" + str(numMat))
-                            numMat += 1
+    # called when the exporter is invoked
+    def invoke(self, context, event):
+        return self.execute(context)
 
-                        numBrush += 1
+    # called by the file selection dialog
+    def execute(self, context):
+        cfg = bpy.data.texts.get('NGFExport.cfg')
+        if cfg:
+            self.save_ngf(cfg.lines[0].body)
+        elif self.filepath != '':
+            self.save_ngf(self.filepath)
+            self.filepath = ''
+        else:
+            context.window_manager.fileselect_add(self)
+            return {'RUNNING_MODAL'}
+        return {'FINISHED'}
 
-                        key = "brushMeshFile"
-                        params = object.getData(1) + ".mesh"
-                        object.select(True)
+    # actually write the file
+    def save_ngf(self, filename):
+        out = open(filename, "w")
 
-                    if (prop.getType() == "STRING"):
-                        #If string check for refText.
-                        if params.startswith("refText"):
-                            textName = params[8:]
-                            text = Blender.Text.Get(textName)
-                            indent = "\t\t\t" + (" " * len(key)) + " "
-                            params = ": "
-                            notFirst = False
-                            for line in text.asLines():
-                                if notFirst:
-                                    params = params + "\n" + indent + ": "
-                                params = params + line
-                                notFirst = True
-                        data = params
-                    else:
-                        #Otherwise convert whatever it is (float, bool, etc.) to a string.
-                        data = str(params)
+        # each scene becomes one NGF level
+        for scene in bpy.data.scenes:
+            out.write("ngflevel %s\n{\n" % (scene.name))
 
-                    propStr = propStr + ("\t\t\t%s %s\n" % (key, data))
+            # brush counter
+            brushIndex = 0
 
-            propStr = propStr + "\t\t}\n"
-            if numProps != 0:
-                out.write(propStr)
+            for obj in scene.objects:
+                # deselect now, reselect later if brush
+                obj.select = False
 
-            out.write("\t}\n")
+                # get position, orientation, game properties
+                obj.rotation_mode = 'QUATERNION'
+                pos = obj.location
+                rot = obj.rotation_quaternion
+                props = obj.game.properties
 
-        scn.setLayers([1,2])
-        Blender.Redraw(-1)
-        out.write("}\n\n")
-        
-    if "NGFExport.cfg" in [text.getName() for text in Blender.Text.Get()]:
-            if ((Blender.Text.Get("NGFExport.cfg").asLines()[0]) != filename):
-                    old = Blender.Text.Get("NGFExport.cfg")
-                    old.setName("NGFExport.cfg.old")
-                    Blender.Text.unlink(old)
-    else:
-            txt = Blender.Text.New("NGFExport.cfg")
-            txt.write(filename)
+                # write basic game properties and position, orientation
+                out.write("\tobject\n\t{\n")
+                out.write("\t\ttype %s\n" % props['type'].value)
+                out.write("\t\tname %s\n" % props['name'].value)
+                out.write("\t\tposition %f %f %f\n" % (pos[0], pos[2], -pos[1]))
+                out.write("\t\trotation %f %f %f %f\n" % (rot[0], rot[1], rot[3], -rot[2]))
 
-if "NGFExport.cfg" in [text.getName() for text in Blender.Text.Get()]:
-        write(Blender.Text.Get("NGFExport.cfg").asLines()[0])
-else:
-        Blender.Window.FileSelector(write, "Export")
+                # write special properties
+                haveProps = False # whether there are any properties - initially no
+                propStr = "\n\t\tproperties\n\t\t{\n"
+
+                # (GRALL 2 MODIFICATION: write dimension value corresponding to obj.layers boolean vector)
+                propStr += "\t\t\tdimensions %d\n" % (functools.reduce(
+                    lambda p, b: (p[0] + 1, (b << p[0]) | p[1]), obj.layers, (0, 0))[1])
+
+                for prop in props:
+                    # (GRALL 2 MODIFICATION: don't write 'dimensions', ... )
+                    if prop.name not in ['type', 'name', 'dimensions', 'dimension1', 'dimension2']:
+                        haveProps = True
+                        name = prop.name
+                        value = ''
+
+                        # check if brush and save relevant info
+                        if name == 'isBrush':
+                            # update name to follow naming convention
+                            brushName = "%s_b%d" % (scene.name, brushIndex)
+                            obj.name = brushName       #object name
+                            obj.data.name = brushName  #mesh name
+
+                            # rename materials to follow naming convention
+                            for matIndex, mat in enumerate([slot.material for slot in obj.material_slots]):
+                                mat.name = '%s_m%d' % (brushName, matIndex)
+
+                            # store mesh name in property
+                            name = "brushMeshFile"
+                            value = obj.data.name + ".mesh"
+                            brushIndex += 1
+
+                            # select for subsequent Ogre .mesh export
+                            obj.select = True
+
+                        # check for refText - reference to Blender text object (useful for scripts etc.)
+                        elif prop.type == 'STRING' and prop.value.startswith('refText'):
+                            print(obj.name)
+                            lines = [line.body for line in bpy.data.texts[prop.value[8:]].lines]
+
+                            # first line is simple
+                            value = ": " + lines[0]
+
+                            # indent other lines
+                            lineFormat = "\n\t\t\t" + (" " * len(name)) + " : %s"
+                            for line in lines[1:]:
+                                value += lineFormat % line
+
+                        # else simply convert to string
+                        else:
+                            value = str(prop.value)
+
+                        # write name, value pair
+                        propStr += "\t\t\t%s %s\n" % (name, value)
+
+                if haveProps:
+                    out.write(propStr + "\t\t}\n")
+
+                out.write("\t}\n")
+
+            # (GRALL 2 MODIFICATION: enable all layers)
+            scene.layers = [True] * len(scene.layers)
+
+            # end of scene
+            out.write("}\n\n")
+
+        # save filename
+        cfg = bpy.data.texts.get('NGFExport.cfg')
+        if cfg:
+            bpy.data.texts.remove(cfg)
+        cfg = bpy.data.texts.new('NGFExport.cfg')
+        cfg.write(filename)
+
+        # we're done!
+        out.close()
+
+# Blender registration
+def menu_func(self, context):
+    self.layout.operator_context = 'INVOKE_DEFAULT'
+    self.layout.operator(GraLL2Exporter.bl_idname, text='GraLL 2 NGF Exporter')
+def register():
+    bpy.utils.register_module(__name__)
+    bpy.types.INFO_MT_file_export.append(menu_func)
+def unregister():
+    bpy.utils.unregister_module(__name__)
+    bpy.types.INFO_MT_file_export.remove(menu_func)
+
+if __name__ == "__main__":
+    register()
